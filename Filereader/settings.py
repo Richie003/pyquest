97a1baa8b41e4e00202a685d9a5dc3f48904b1dc
Dilosi Richfield <richiedilosi2003@gmail.com>
2023-02-24 13:00:19 +0100

    Update settings.py

--- Filereader/settings.py
+++ Filereader/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ooez#6bjesw!)fs4w@317*8ln8egc#mu3j$*4temtvy$+r9!sa'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
